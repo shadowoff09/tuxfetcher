876d4837761956fab668d8ab5ebb099608124738
Diogo Gaspar <diogogaspar931@gmail.com>
2024-06-03 19:34:04 +0100

    Implemented DNS Server Installation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from bind import install_bind
+from bind import install_bind, uninstall_bind
 from sys import platform
 
 
@@ -9,15 +9,14 @@ def display_menu():
     print("Select an option:")
     print("===============Bind DNS Server================")
     print("1. Install BIND DNS Server")
-    print("2. Revert Changes (Uninstall BIND DNS Server)")
+    print("2. Uninstall BIND DNS Server")
     print("0. Exit")
 
     choice = input("Enter your choice: ").strip()
     if choice == "1":
         install_bind()
     elif choice == "2":
-        print("Error: Feature not yet implemented.")
-        exit(0)
+        uninstall_bind()
     elif choice == "0":
         print("Exiting...")
         exit(0)
